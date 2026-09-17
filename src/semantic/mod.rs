e8b485523a1e6b0cc4a58610bee254ca5865cff9
arun_murali0 <arunkarthick0810@gmail.com>
2026-09-17 17:55:54 +0530

    added a method in semantic mod

--- src/semantic/mod.rs
+++ src/semantic/mod.rs
@@ -1,5 +1,8 @@
 mod generics;
 pub mod queries;
 
-pub(crate) use generics::{expected_param_type, infer_type_param_bindings, substitute_type_params};
+pub(crate) use generics::{
+    collect_generic_param_constraints, expected_param_type, infer_type_param_bindings,
+    substitute_type_params,
+};
 pub use queries::SemanticQueries;
